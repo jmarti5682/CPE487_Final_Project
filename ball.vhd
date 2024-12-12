06e0889d1c71ba068530fe51b900ea519dab0cf7
Hose <101851045+jmarti5682@users.noreply.github.com>
2024-12-12 13:06:01 -0500

    Update ball.vhd
    focus on a smaller approach, made it to one ball and then add the bottom oval

--- ball.vhd
+++ ball.vhd
@@ -16,27 +16,23 @@ ENTITY ball IS
 END ball;
 
 ARCHITECTURE Behavioral OF ball IS
-	CONSTANT num_balls : INTEGER := 4; -- NUMBER of balls
+	-- CONSTANT num_balls : INTEGER := 4;  -- NUMBER of balls
+	CONSTANT oval_height : INTEGER := 30;  -- height of oval
+	CONSTANT oval_width : INTEGER := 15;  -- width of oval
+
+	CONSTANT screen_height : INTEGER := 600;  -- Screen Height
+	CONSTANT screen_width : INTEGER := 800;  -- Screen Width
+
+	CONSTANT bottom_position : INTEGER := 550;  -- Bottom button position (In Pixel)
+
 	CONSTANT size : INTEGER := 20; -- Size of each ball
+
+	SIGNAL ball_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR (400, 11);  -- Ball's X position
+	SIGNAL ball_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR (0, 11);  -- Ball's Y Position
+	SIGNAL ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR (4, 11);  -- Ball's Falling Speed
 	SIGNAL ball_on : STD_LOGIC; -- indicates whether ball is over current pixel position
+	SIGNAL oval_on : STD_LOGIC;
 
--- Ball Attributes  (easier to set "settings" for each ball)
-	TYPE ball_position_arr IS ARRAY (0 TO num_balls - 1) OF STD_LOGIC_VECTOR(10 DOWNTO 0);
-	TYPE ball_on_array IS ARRAY (0 TO num_balls - 1) OF STD_LOGIC;
-
-	SIGNAL ball_x : ball_position_array := (
-		CONV_STD_LOGIC_VECTOR(100, 11), -- ball 1 x
-		CONV_STD_LOGIC_VECTOR(200, 11), -- ball 2 x
-		CONV_STD_LOGIC_VECTOR(300, 11), -- ball 3 x
-		CONV_STD_LOGIC_VECTOR(400, 11), -- ball 4 x
-	);
-
-	SIGNAL ball_y : ball_position_array := (
-		CONV_STD_LOGIC_VECTOR(0, 11), -- ball 1 y
-		CONV_STD_LOGIC_VECTOR(0, 11), -- ball 2 y
-		CONV_STD_LOGIC_VECTOR(0, 11), -- ball 3 y
-		CONV_STD_LOGIC_VECTOR(0, 11), -- ball 4 y
-	);
 
 -- current ball position - intitialized to center of screen
 	--SIGNAL ball_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
@@ -56,30 +52,37 @@ ARCHITECTURE Behavioral OF ball IS
 	bdraw : PROCESS (pixel_row,pixel_col)
 	BEGIN
 		-- (ball_x-pixel_col)^2 + (ball_ypixel_row)^2 = size^2
-		FOR i IN 0 TO num_balls - 1 LOOP
-			IF ((conv_integer(ball_x(i)) - conv_integer(pixel_col))**2 + 
-			(conv_integer(ball_y(i)) - conv_integer(pixel_row))**2 
-			<= size**2) THEN
-				ball_on(i) <= '1';
-			ELSE
-				ball_on(i) <= '0';
-			END IF;
-		END LOOP;
-	END PROCESS;
-			-- process to move ball once every frame (i.e. once every vsync pulse)
-	mball : PROCESS
-	BEGIN
-		WAIT UNTIL rising_edge(v_sync);
-		FOR i IN 0 TO num_balls-1 LOOP
-			-- Move ball down
-			ball_y(i) <= ball_y(i) + ball_y_motion;
-
-			-- Reset ball to the top if it goes off the bottom of the screen
-			IF conv_integer(ball_y(i)) >= screen_height THEN
-				ball_y(i) <= CONV_STD_LOGIC_VECTOR(0, 11); -- Reset Y to the top
-				-- Optionally, randomize or cycle the X position for variety
-				ball_x(i) <= CONV_STD_LOGIC_VECTOR((conv_integer(ball_x(i)) + 200) MOD screen_width, 11);
+		IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 + 
+		(conv_integer(ball_y) - conv_integer(pixel_row))**2 
+		<= size**2) THEN
+			ball_on <= '1';
+		ELSE
+			ball_on <= '0';
 		END IF;
-		END LOOP;
-	END PROCESS;
+
+		-- Oval Shape
+
+		IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 / oval_width**2 +
+			(conv_integer(ball_y) - conv_integer(pixel_row))**2 / oval_height**2
+			<= 1) THEN
+			oval_on <= '1';
+		ELSE
+			oval_on <= '0';
+		END IF;
+	
+-- Process to update ball position on every v_sync pulse
+mball : PROCESS
+BEGIN
+	WAIT UNTIL rising_edge(v_sync);
+	-- Move ball down
+	ball_y <= ball_y + ball_y_motion;
+
+	-- Reset ball to the top if it goes off the bottom of the screen
+	IF conv_integer(ball_y) >= screen_height THEN
+		ball_y <= CONV_STD_LOGIC_VECTOR(0, 11); -- Reset Y to the top
+	END IF;
+END PROCESS;
+
+
+END PROCESS;
 END Behavioral;
