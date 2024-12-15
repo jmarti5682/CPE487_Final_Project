0a6d18e75faf11df6452ba8b9e940aa864e2082b
Hose <101851045+jmarti5682@users.noreply.github.com>
2024-12-15 13:41:15 -0500

    Update ball.vhd

--- ball.vhd
+++ ball.vhd
@@ -16,11 +16,11 @@ ENTITY ball IS
 		-- Colors
 		red : OUT STD_LOGIC;
 		green : OUT STD_LOGIC;
-		blue : OUT STD_LOGIC
+		blue : OUT STD_LOGIC;
 
 		-- key pad
 		keypad_row : IN STD_LOGIC_VECTOR (4 DOWNTO 1); -- keypad row pins
-		keypad_col : OUT STD_LOGIC_VECTOR (4 DOWNTO 1); -- keypad column pins
+		keypad_col : OUT STD_LOGIC_VECTOR (4 DOWNTO 1) -- keypad column pins
 	);
 END ball;
 
@@ -28,8 +28,8 @@ END ball;
 ARCHITECTURE Behavioral OF ball IS
 	-- CONSTANT num_balls : INTEGER := 4;  -- NUMBER of balls
 
-	CONSTANT oval_height : INTEGER := 30;  -- height of oval
-	CONSTANT oval_width : INTEGER := 15;  -- width of oval
+	CONSTANT oval_height : INTEGER := 40;  -- height of oval
+	CONSTANT oval_width : INTEGER := 50;  -- width of oval
 
 	CONSTANT screen_height : INTEGER := 600;  -- Screen Height
 	CONSTANT screen_width : INTEGER := 800;  -- Screen Width
@@ -62,10 +62,10 @@ ARCHITECTURE Behavioral OF ball IS
 
 	BEGIN
 		ck_proc : PROCESS (clk_50MHz)
-		BEGIN
-			IF rising_edge(clk_50MHz) THEN
-				cnt <= cnt + 1;
-			END IF;
+            BEGIN
+                IF rising_edge(clk_50MHz) THEN
+                    cnt <= cnt + 1;
+                END IF;
 		END PROCESS;
 
 		kp_clk <= cnt(15);
@@ -77,7 +77,7 @@ ARCHITECTURE Behavioral OF ball IS
 			samp_ck => kp_clk, col => keypad_col,
 			row => keypad_row, value => kp_value, hit => kp_hit
 		);
-	END PROCESS;
+
 -- current ball position - intitialized to center of screen
 	--SIGNAL ball_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
 	--SIGNAL ball_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
@@ -85,45 +85,41 @@ ARCHITECTURE Behavioral OF ball IS
 -- current ball motion - initialized to +4 pixels/frame
 	--SIGNAL ball_x_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000100";
 	--SIGNAL ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000100";
-	BEGIN
+
 		red <= NOT ball_on; -- color setup for red ball on white background
 		green <= NOT oval_on; --NOT ball_on;
 		blue <= '1'; --NOT ball_on;
-
-
 	-- process to draw ball current pixel address is covered by ball position
-	bdraw : PROCESS (pixel_row,pixel_col)
-	BEGIN
-		-- (ball_x-pixel_col)^2 + (ball_ypixel_row)^2 = size^2
-		IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 + 
-		(conv_integer(ball_y) - conv_integer(pixel_row))**2 
-		<= size**2) THEN
-			ball_on <= '1';
-		ELSE
-			ball_on <= '0';
-		END IF;
-
-		-- Oval Shape
-		IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 / oval_width**2 +
-			(conv_integer(ball_y) - conv_integer(pixel_row))**2 / oval_height**2
-			<= 1) THEN
-			oval_on <= '1';
-		ELSE
-			oval_on <= '0';
-		END IF;
-	END PROCESS;
--- Process to update ball position on every v_sync pulse
-    mball : PROCESS
-    BEGIN
-	WAIT UNTIL rising_edge(v_sync);
-	-- Move ball down
-	ball_y <= ball_y + ball_y_motion;
-
-	-- Reset ball to the top if it goes off the bottom of the screen
-	IF conv_integer(ball_y) >= screen_height THEN
-		ball_y <= CONV_STD_LOGIC_VECTOR(0, 11); -- Reset Y to the top
-	END IF;
-END PROCESS;
-
-
+        bdraw : PROCESS (pixel_row,pixel_col)
+        BEGIN
+            -- (ball_x-pixel_col)^2 + (ball_ypixel_row)^2 = size^2
+            IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 + 
+            (conv_integer(ball_y) - conv_integer(pixel_row))**2 
+            <= size**2) THEN
+                ball_on <= '1';
+            ELSE
+                ball_on <= '0';
+            END IF;
+    
+            -- Oval Shape
+            IF ((conv_integer(ball_x) - conv_integer(pixel_col))**2 / oval_width**2 +
+                (conv_integer(bottom_position) - conv_integer(pixel_row))**2 / oval_height**2
+                <= 1) THEN
+                oval_on <= '1';
+            ELSE
+                oval_on <= '0';
+            END IF;
+        END PROCESS;
+    -- Process to update ball position on every v_sync pulse
+        mball : PROCESS
+        BEGIN
+        WAIT UNTIL rising_edge(v_sync);
+        -- Move ball down
+        ball_y <= ball_y + ball_y_motion;
+    
+        -- Reset ball to the top if it goes off the bottom of the screen
+        IF conv_integer(ball_y) >= screen_height THEN
+            ball_y <= CONV_STD_LOGIC_VECTOR(0, 11); -- Reset Y to the top
+        END IF;
+    END PROCESS;
 END Behavioral;
